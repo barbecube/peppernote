6ac4d2e1e339a3d8ffb620f67a72a694fb2d3b64
Jakub Barbierik <jakub.barbierik2@gmail.com>
2012-04-18 21:20:23 +0200

    screen.css & print.css precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Peppernote::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
